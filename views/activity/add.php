b5a503fb3f6d5fc4380fe1813c15547a60d2175a
Pavel <leniviy.putnik@gmail.com>
2019-12-05 00:15:01 +0400

    Сгенерированы разные формы

--- views/activity/add.php
+++ views/activity/add.php
@@ -5,6 +5,8 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use kartik\date\DatePicker;
+
 
 $this->title = 'Добавление события';
 $this->params['breadcrumbs'][] = $this->title;
@@ -15,8 +17,23 @@ $this->params['breadcrumbs'][] = $this->title;
     $form = ActiveForm::begin(['method' => 'post', 'action'=>['activity/submit']]);
     echo $form->field($model, 'name')->textInput(['autofocus' => true]);
     echo $form->field($model, 'description')->textarea();
-    echo $form->field($model, 'start')->textInput();
-    echo $form->field($model, 'finish')->textInput();
+    echo $form->field($model, 'start')->widget(DatePicker::classname(), [
+        'options' => ['placeholder' => 'Введите дату начала...'],
+        'pluginOptions' => [
+            'autoclose'=>true,
+            'language'=>'ru',
+            'format' => 'dd-MM-yyyy',
+        ]
+    ]);
+    echo $form->field($model, 'finish')->widget(DatePicker::classname(), [
+        'options' => ['placeholder' => 'Введите дату окончания...'],
+        'pluginOptions' => [
+            'autoclose'=>true,
+            'language'=>'ru',
+            'format' => 'dd-MM-yyyy',
+        ]
+    ]);
+
     echo $form->field($model, 'repeatable')->checkbox([
         'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",]);
     echo $form->field($model, 'blocker')->checkbox([
